a3694f73d3c546de56a962020b3284ec3f228892
ms548 <ms548@duke.edu>
2016-10-19 16:40:25 -0400

    change how command parser is created

--- src/controller/SLogoController.java
+++ src/controller/SLogoController.java
@@ -7,38 +7,32 @@ import java.util.stream.Collectors;
 import model.CommandParser;
 import model.command.*;
 
+
 public class SLogoController {
-    
+
     private CommandParser myCommandParser;
-    
-    public SLogoController(){
+
+    public SLogoController () {
         myCommandParser = new CommandParser("English");
-        
+
     }
-    
+
     public void parseText (String[] text) {
-        
+
         for (String s : text) {
-            
+
             if (s.trim().length() > 0) {
-                
-                System.out.println(String.format("%s : %s", s, myCommandParser.getSymbol(s)));
-                AbstractCommand c = null;
-                try{
-                    c = myCommandParser.parse(s);
+
+                try {
+                    System.out.println(myCommandParser.parse(s));
                 }
-                catch(ClassNotFoundException e){
+                catch (Exception e) {
                     e.printStackTrace();
                 }
-                if(c!=null){
-                    System.out.println(c.toString());
-                }
-                
+
             }
         }
-        
-        
-        
+
         System.out.println();
     }
 }
