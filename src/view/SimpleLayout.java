ff8ed21b80cd84d00d3f80f98e9fa2ac14686e0b
Will Long <wlong799@gmail.com>
2016-10-19 20:34:29 -0400

    Added LayoutManager interface for setting up GUI
    LayoutManager interface will provide a flexible way to provide multiple
    different layouts for the CellSocietyGUI. This will make it easy to
    create applications with different elements present, or to lay them
    out in a different manner.

---
+++ src/view/SimpleLayout.java
@@ -0,0 +1,16 @@
+package view;
+
+import javafx.scene.Parent;
+
+import java.util.List;
+
+/**
+ * @author Will Long
+ * @version 10/19/16
+ */
+public class SimpleLayout implements LayoutManager{
+    @Override
+    public Parent setComponentLayout(List<ViewElement> viewElements) {
+        return null;
+    }
+}
