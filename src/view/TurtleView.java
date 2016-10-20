91b3fcf2b6b644cd8fcf760e7bfa345dcc97893e
Will Long <wlong799@gmail.com>
2016-10-19 22:45:02 -0400

    Added an image for the turtle

--- src/view/TurtleView.java
+++ src/view/TurtleView.java
@@ -3,6 +3,7 @@ package view;
 import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -13,7 +14,9 @@ import javafx.scene.shape.Rectangle;
  * @version 10/19/16
  */
 public class TurtleView implements ViewElement {
-    private static final Color DEFAULT_BG_COLOR = Color.BLACK;
+    private static final Color DEFAULT_BG_COLOR = Color.WHITE;
+    private static final String TURTLE_IMAGE_LOCATION = "resources/turtle.png";
+    private static final double TURTLE_SIZE = 50;
 
     private StackPane myContent;
     private Rectangle background;
@@ -31,7 +34,9 @@ public class TurtleView implements ViewElement {
         background = new Rectangle(width, height, DEFAULT_BG_COLOR);
         lineCanvas = new Canvas(width, height);
         lineGraphics = lineCanvas.getGraphicsContext2D();
-        turtle = new ImageView();
+        turtle = new ImageView(new Image("resources/turtle2.png"));
+        turtle.setFitHeight(TURTLE_SIZE);
+        turtle.setFitWidth(TURTLE_SIZE);
 
         lineGraphics.setFill(Color.YELLOW);
         lineGraphics.fillRect(0, 0, myWidth, myHeight/2);
