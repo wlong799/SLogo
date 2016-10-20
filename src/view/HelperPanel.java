07b6b84b91339d226d301539a716b9bb5d967607
Will Long <wlong799@gmail.com>
2016-10-19 20:26:50 -0400

    Laid out basic classes to be created for GUI
    All elements implement the interface ViewElement. This allows for other
    classes to access the components of the GUI in a more general fashion,
    and makes the code flexible by providing a simple getContent method that
    can easily be changed in case the implementation details of the element
    change.

---
+++ src/view/HelperPanel.java
@@ -0,0 +1,14 @@
+package view;
+
+import javafx.scene.Node;
+
+/**
+ * @author Will Long
+ * @version 10/19/16
+ */
+public class HelperPanel implements ViewElement{
+    @Override
+    public Node getContent() {
+        return null;
+    }
+}
---
+++ src/view/StoredFunctionWindow.java
@@ -0,0 +1,14 @@
+package view;
+
+import javafx.scene.Node;
+
+/**
+ * @author Will Long
+ * @version 10/19/16
+ */
+public class StoredFunctionWindow implements ViewElement{
+    @Override
+    public Node getContent() {
+        return null;
+    }
+}
