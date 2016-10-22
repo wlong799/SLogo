3bfee5c790313ece0643edcfbee565b776a786b6
ms548 <ms548@duke.edu>
2016-10-22 15:30:37 -0400

    add Variable command class; allows variables to be parsed the same way, and only evaluated at execution time

--- src/model/command/Variable.java
+++ src/model/command/Variable.java
@@ -1,5 +1,35 @@
 package model.command;
 
+import java.util.List;
+import model.ExpressionNode;
 
-public class Variable {
+
+public class Variable extends AbstractCommandHigherOrder {
+
+    private String myVariableName;
+
+    public Variable (String name) {
+        super();
+        myVariableName = name;
+    }
+
+    public Variable (List<ExpressionNode> parameters) {
+        super(parameters);
+    }
+
+    @Override
+    public String toString () {
+        return myVariableName;
+    }
+
+    @Override
+    public double execute () {
+        return getVariables().getVariable(myVariableName);
+    }
+
+    @Override
+    public int getNumParameters () {
+        return 0;
+    }
+
 }
