691e4f53d2339ee3e6e2324aa2ff69ba33e6f382
Will Long <wpl5@duke.edu>
2016-10-27 16:18:32 -0400

    Added class for managing and getting view elements

---
+++ src/view/ViewElementManager.java
@@ -0,0 +1,34 @@
+package view;
+
+import view.element.Viewable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Responsible for storing all current elements present within the GUI, and providing access to them.
+ */
+public class ViewElementManager {
+    private List<Viewable> myViewElements;
+
+    public ViewElementManager() {
+        myViewElements = new ArrayList<>();
+    }
+
+    public void addElement(Viewable element) {
+        myViewElements.add(element);
+    }
+
+    public Viewable getElement(String className) {
+        for (Viewable element : myViewElements) {
+            if (checkClass(element, className)) {
+                return element;
+            }
+        }
+        return null;
+    }
+
+    private boolean checkClass(Viewable element, String className) {
+        return element.getClass().getSimpleName().equals(className);
+    }
+}
