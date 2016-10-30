ccd1c395cddb7fd96b2fbd9b0346b643d2af545c
ms548 <ms548@duke.edu>
2016-10-29 20:59:16 -0400

    add ask command

---
+++ src/model/command/twoParameter/turtle/Ask.java
@@ -0,0 +1,35 @@
+package model.command.twoParameter.turtle;
+
+import java.util.ArrayList;
+import java.util.List;
+import dataStorage.TurtleStorage;
+import model.command.AbstractCommand;
+import model.command.ListCommand; 
+
+
+public class Ask extends AbstractCommandTwoParameterTurtle {
+
+    public Ask (List<AbstractCommand> parameters) {
+        super(parameters);
+    }
+
+    @Override
+    public double execute () {
+        return turtleExecute();
+    }
+
+    @Override
+    protected double turtleExecute () {
+        TurtleStorage turtles = getTurtles();
+        List<Integer> oldTurtles = new ArrayList<Integer>(turtles.getActiveTurtleIDs());
+        ListCommand idCommand = (ListCommand)getRawParameters().get(PARAMETER_ONE);
+        List<Double> newTurtles = idCommand.getParameters();
+        List<Integer> newTurtleIDs = new ArrayList<Integer>();
+        newTurtles.stream().forEach(d -> newTurtleIDs.add(d.intValue()));
+        turtles.setActiveTurtles(newTurtleIDs);
+        double output = getRawParameters().get(PARAMETER_TWO).execute();
+        turtles.setActiveTurtles(oldTurtles);
+        return output;
+    }
+
+}
