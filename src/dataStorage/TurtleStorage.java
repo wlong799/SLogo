5e9805dcbde8915131811b3d51afca548535e25d
ms548 <ms548@duke.edu>
2016-10-31 00:10:30 -0400

    multiple turtles

--- src/dataStorage/TurtleStorage.java
+++ src/dataStorage/TurtleStorage.java
@@ -5,9 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+
 
 /**
  * 
@@ -18,20 +18,23 @@ public class TurtleStorage extends Observable implements Observer {
 
     private HashMap<Integer, Turtle> turtleMap;
     private ObservableList<Integer> activeTurtles;
+    private List<TurtleState> changedTurtles;
 
-    public TurtleStorage() {
+    public TurtleStorage () {
         turtleMap = new HashMap<>();
         activeTurtles = FXCollections.observableArrayList();
+        changedTurtles = new ArrayList<TurtleState>();
     }
 
-    public void setActiveTurtles(List<Integer> ids) {
+    public void setActiveTurtles (List<Integer> ids) {
         activeTurtles.clear();
         System.out.println("Active turtles cleared. Active turtles = " + activeTurtles);
         for (int id : ids) {
             System.out.println("Adding turtle " + id + " to active");
             if (turtleMap.containsKey(id)) {
                 activeTurtles.add(id);
-            } else {
+            }
+            else {
                 Turtle t = new Turtle(id);
                 t.addObserver(this);
                 turtleMap.put(id, t);
@@ -41,7 +44,7 @@ public class TurtleStorage extends Observable implements Observer {
         }
     }
 
-    public List<Turtle> getActiveTurtles() {
+    public List<Turtle> getActiveTurtles () {
         List<Turtle> turtles = new ArrayList<>();
         for (int i : activeTurtles) {
             turtles.add(turtleMap.get(i));
@@ -49,17 +52,24 @@ public class TurtleStorage extends Observable implements Observer {
         return turtles;
     }
 
-    public ObservableList<Integer> getActiveTurtleIDs() {
+    public ObservableList<Integer> getActiveTurtleIDs () {
         return activeTurtles;
     }
 
     @Override
-    public void update(Observable o, Object arg) {
+    public void update (Observable o, Object arg) {
         System.out.println("CHANGED");
         System.out.println(o);
         TurtleState state = (TurtleState) arg;
         System.out.println(state.getPosition());
+        changedTurtles.add(state);
+        // setChanged();
+        // notifyObservers(arg);
+    }
+
+    public void updateTurtles () {
         setChanged();
-        notifyObservers(arg);
+        notifyObservers(changedTurtles);
+        changedTurtles.clear();
     }
 }
