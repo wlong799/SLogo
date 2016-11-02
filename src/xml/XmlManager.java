231c05604ff42c60e090c1189b35a49bb0f94ae4
ms548 <ms548@duke.edu>
2016-11-02 01:51:10 -0400

    fix bug of overwriting data storage

--- src/xml/XmlManager.java
+++ src/xml/XmlManager.java
@@ -1,21 +1,24 @@
 package xml;
 
 import controller.workspace.WorkspaceLoadPreferences;
+import dataStorage.CommandStorage;
 import dataStorage.DataStorageManager;
+import dataStorage.VariableStorage;
 import exceptions.XmlFormatException;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 
 public class XmlManager implements IXmlStrings {
 
-    public void saveCommandsVariables(DataStorageManager dataStorage) throws Exception{
+    public void saveCommandsVariables (DataStorageManager dataStorage) throws Exception {
         XmlSaver saver = new XmlSaver();
         saver.saveCommandsVariables(dataStorage);
     }
 
-    public WorkspaceLoadPreferences loadWorkspacePreferences(File givenXml) throws Exception {
+    public WorkspaceLoadPreferences loadWorkspacePreferences (File givenXml) throws Exception {
         XmlParser parser = new XmlParser();
         Map<String, Object> parsedMap = parser.XmlParse(givenXml);
 
@@ -32,7 +35,8 @@ public class XmlManager implements IXmlStrings {
         return setter.setWorkspaceLoadPreferences(workspaceMap);
     }
 
-    public void loadAndSetVariablesCommands(File givenXml, DataStorageManager dataStorage) throws Exception {
+    public void loadAndSetVariablesCommands (File givenXml,
+                                             DataStorageManager dataStorage) throws Exception {
         XmlParser parser = new XmlParser();
         Map<String, Object> parsedMap = parser.XmlParse(givenXml);
 
@@ -40,15 +44,15 @@ public class XmlManager implements IXmlStrings {
         try {
             tempMap = (Map<String, Object>) parsedMap.get(SAVED_COMMANDS_VARIABLES);
         }
-        catch(NullPointerException e) {
+        catch (NullPointerException e) {
             throw new XmlFormatException(SAVED_COMMANDS_VARIABLES);
         }
 
-        Map<String,  Map<String, String>> variableMap = new HashMap<>();
-        try{
-            variableMap = (Map<String,  Map<String, String>>) tempMap.get(VARIABLE_STORAGE);
+        Map<String, Map<String, String>> variableMap = new HashMap<>();
+        try {
+            variableMap = (Map<String, Map<String, String>>) tempMap.get(VARIABLE_STORAGE);
         }
-        catch(NullPointerException e) {
+        catch (NullPointerException e) {
             throw new XmlFormatException(VARIABLE_STORAGE);
         }
 
@@ -56,14 +60,35 @@ public class XmlManager implements IXmlStrings {
         try {
             commandMap = (Map<String, Map<String, String>>) tempMap.get(FUNCTION_STORAGE);
         }
-        catch(NullPointerException e) {
+        catch (NullPointerException e) {
             throw new XmlFormatException(FUNCTION_STORAGE);
         }
 
         XmlDataSetter setter = new XmlDataSetter();
-
-        dataStorage.setVariableStorage(setter.setValueVariables(variableMap));
-        dataStorage.setCommandStorage(setter.setCommandVariables(commandMap));
+        VariableStorage variables = setter.setValueVariables(variableMap);
+        CommandStorage commands = setter.setCommandVariables(commandMap);
+        addDataToStorage(dataStorage, variables, commands);
+        // dataStorage.setVariableStorage(setter.setValueVariables(variableMap));
+        // dataStorage.setCommandStorage(setter.setCommandVariables(commandMap));
         System.out.println("Set new Variables and commands from file");
     }
+
+    /**
+     * @param dataStorage
+     * @param variables
+     * @param commands
+     */
+    private void addDataToStorage (DataStorageManager dataStorage,
+                                   VariableStorage variables,
+                                   CommandStorage commands) {
+        for (Entry<String, Double> entry : variables.getVariableMap().entrySet()) {
+            dataStorage.setVariable(entry.getKey(), entry.getValue());
+        }
+
+        for (Entry<String, String> entry : commands.getCommandMap().entrySet()) {
+            dataStorage.setCommand(entry.getKey(), commands.getCommandParams(entry.getKey()),
+                                   entry.getValue());
+        }
+    }
+
 }
--- src/xml/XmlParser.java
+++ src/xml/XmlParser.java
@@ -11,7 +11,8 @@ import org.xml.sax.SAXException;
 class XmlParser {
     private static final String BAD_TEXT_NODE = "#text";
 
-    Map<String, Object> XmlParse(File file) throws SAXException, IOException, ParserConfigurationException{
+    Map<String, Object> XmlParse (File file) throws SAXException, IOException,
+                                             ParserConfigurationException {
         DocumentBuilderFactory dbFactory =
                 DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder;
@@ -27,23 +28,28 @@ class XmlParser {
         return rules;
     }
 
-    //    http://stackoverflow.com/questions/15759796/xml-recursive-node-parser
-    private Object childMapper(Node root, Node parent) {
+    // http://stackoverflow.com/questions/15759796/xml-recursive-node-parser
+    private Object childMapper (Node root, Node parent) {
+        if (root == null) {
+            System.out.println("root is null");
+            return null;
+        }
         if (root.hasChildNodes()) {
 
             HashMap<String, Object> childMap = new HashMap<String, Object>();
 
-//            System.out.println(root.getNodeName());
+            // System.out.println(root.getNodeName());
             NodeList children = root.getChildNodes();
             for (int i = 0; i < children.getLength(); i++) {
-                if(!(children.item(i).getNodeName().equals(BAD_TEXT_NODE))){
-                    childMap.put(children.item(i).getNodeName(), childMapper(children.item(i), root));
+                if (!(children.item(i).getNodeName().equals(BAD_TEXT_NODE))) {
+                    childMap.put(children.item(i).getNodeName(),
+                                 childMapper(children.item(i), root));
                 }
 
-                if(children.item(i).getNodeName().equals(BAD_TEXT_NODE)){
+                if (children.item(i).getNodeName().equals(BAD_TEXT_NODE)) {
                     String nodeValue = children.item(i).getNodeValue().trim();
                     if (nodeValue.length() > 0) {
-//                        System.out.println(parent.getNodeName() + "::" + nodeValue);
+                        // System.out.println(parent.getNodeName() + "::" + nodeValue);
                         return childMapper(children.item(i), root);
                     }
                 }
@@ -52,9 +58,10 @@ class XmlParser {
             return childMap;
         }
         else {
+            System.out.println(root.getNodeValue());
             String nodeValue = root.getNodeValue().trim();
             if (nodeValue.length() > 0) {
-//                System.out.println(parent.getNodeName() + "::" + nodeValue);
+                // System.out.println(parent.getNodeName() + "::" + nodeValue);
                 return nodeValue;
             }
         }
